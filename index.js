8f1595135d67d9139121d605e9ef39ea86528df6
Keenan Barber <keenan.c.barber@gmail.com>
2017-12-09 13:29:41 -0500

    First test works with TDD. Function checks if time is not between 4am and 5pm using 24 hour system. Does not yet check for values not between 0-24. Will implement next.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 module.exports = {
-
+    startTime: function(start) {
+        let goodStartTime = true;
+        if (4 < start < 17) {
+            return !goodStartTime;
+        }
+        return goodStartTime;
+    },
     sayHello: function() {
-        return 'hello';
+        return "hello";
     },
     addNumber: function(num1, num2) {
         return num1 + num2;
     }
 };
-
--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const  assert = require("chai").assert;
 const sayHello = require("./index.js").sayHello;
 const addNumber = require("./index.js").addNumber;
+const startTime = require("./index.js").startTime;
 const app = require("./index.js");
+
 
 describe ("App", function() {
     it("App should return hello", function() {
@@ -19,5 +21,13 @@ describe ("App", function() {
        it("addNumber should return a number", function() {
         let result = addNumber();
         assert.typeOf(result, "number");
+    });
+       it("Start time should be between 5pm and 4am", function() {
+        let result = startTime(12);
+        assert.equal(result, false);
+        it("Start time should be between 5pm and 4am", function() {
+        result = startTime(18);
+        assert.equal(result, true);
+    });
     });
 });
